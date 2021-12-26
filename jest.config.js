0fef006ff0294456afddc39c2c29e6770fad9799
adr1an <adrn.cederberg123@gmail.com>
2021-12-26 15:20:39 -0700

    Reconfigured jest again.

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testMatch : [
+    "**/*.test.ts"
+  ],
+  testEnvironment: 'node',
+  testTimeout : 100000 ,
+  bail: 1,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8"
+};
