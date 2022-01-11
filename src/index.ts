9b54230113e6f5ed0b73f9d6da8f6d9c8ea2c9bc
adr1an <adrn.cederberg123@gmail.com>
2022-01-10 17:12:44 -0700

    Moved 'fetcher' to 'fetchers' to be consistant with naming.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-import { Express } from 'express'
+import { Express, Server } from 'express'
 
 import create_everything from "./app"
 
 
-export default async function main() : Express
+export default async function main() : Promise<[ Express, Server ]>
 {
 
 	const { app } = await create_everything()
 	const port : number = Number( process.env.PORT ) || 8000
 
-	app.listen(
+	return [ 
+		app, 
+		app.listen(
 			port,
 			() => console.log( `Listening on ${ port }.` )
-	)
-
-	return app
+		)
+	]
 
 }
 
