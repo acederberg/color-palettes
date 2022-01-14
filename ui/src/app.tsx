dc6e0aaae174f0ab053b0e32035f1a99f3adac47
adr1an <adrn.cederberg123@gmail.com>
2022-01-14 13:56:58 -0700

    Added basics of app.tsx.

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -1,15 +1,34 @@
-import { Message } from './components'
+import { useState } from 'react'
+
+import { GlobalStateProvider, GlobalState, Pallete, Collection, Collections } from './components'
 
 
-const APP_MESSAGE_NAME : String = "React+Typescript+Webpack Boilerplate"
-const APP_MESSAGE : String = "It works!"
+const GLOBAL_STATE = new GlobalState( 'collection' )  
 
 
 export default function App()
 {
-	return <Message 
-		msg = { APP_MESSAGE } 
-		msg_name = { APP_MESSAGE_NAME } 
-		text_color = '#338aff'
-	/>
+
+	// Uses the global context to determine which components 
+	// to render and the arguements to be passed to it.
+
+	const [ mode, setMode ] = useState( 'collection' )
+	GLOBAL_STATE.invokeGlobalStateUpdate = setMode
+
+	return <GlobalStateProvider initValue = { GLOBAL_STATE }>
+		{
+			mode === 'collections'
+			? <Collections/>
+			: 
+				mode === 'pallete' 
+				? <Collection 
+					collectionName = { GLOBAL_STATE.collectionName }
+				/>
+				: <Pallete 
+					palleteId = { GLOBAL_STATE.palleteId } 
+					palleteCollectionName = { GLOBAL_STATE.collectionName }
+				/>
+		}
+	</GlobalStateProvider>
+
 }
